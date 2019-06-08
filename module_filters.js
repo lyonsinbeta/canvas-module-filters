afd7fec398c4af6ed96e553f8f4ab566e6cb6421
David Lyons <hello@davidlyons.net>
2019-06-07 23:03:41 -0600

    Icons switch between green and grey when toggled

--- module_filters.js
+++ module_filters.js
@@ -1,28 +1,28 @@
 $(document).ready(function() {
   if (window.location.pathname.includes("modules")) {
 
-    $("div.header-bar").append("<ul id='module_filters' style='list-style-type: none;'></ul>");
+    $("div.header-bar").append("<div><ul id='module_filters' style='list-style-type: none; display: inline;'></ul></div>");
 
     var item_types = [{id: "wiki_page", label: "Pages", icon: "icon-document"},
                       {id: "assignment", label: "Assignments", icon: "icon-assignment"},
                       {id: "quiz", label: "Quizzes", icon: "icon-quiz"},
-                      {id: "discussion_topic", label: "Discussions", icon: "icon-discussion"},
+                      {id: "discussion_topic", label: "Discussion Topics", icon: "icon-discussion"},
                       {id: "external_url", label: "Links", icon: "icon-link"},
                       {id: "attachment", label: "Files", icon: "icon-paperclip"}];
 
     item_types.forEach(function(type) {
-      var icon = `<i id="module_filter_${type['id']}" class="${type['icon']}" style="color: green;"></i>`;
+      var icon = `<i id="module_filter_${type['id']}" class="${type['icon']}" title="${type['label']}" style="color: green;"></i>`;
 
-      $("ul#module_filters").append(`<li style="padding-right: 1em; display: inline-block;"><input type="checkbox" id="${type['id']}" name="${type['id']}" checked style="display: none;"> <label for="${type['id']}">${icon}</label></li>`);
+      $("ul#module_filters").append(`<li style="padding: 0 1em 0 0; display: inline-block;"><input type="checkbox" id="${type['id']}" name="${type['id']}" checked style="display: none;"> <label for="${type['id']}">${icon}</label></li>`);
 
-      $("#" + type['id'] + "").change(function() {
+      $(`#${type['id']}`).change(function() {
         if (this.checked == true) {
-          $("li." + type['id'] + "").show();
-          $("#module_filter_" + type['id']).css('color', 'green');
+          $(`li.${type['id']}`).show();
+          $(`#module_filter_${type['id']}`).css('color', 'green');
         }
         else {
-          $("li." + type['id'] + "").hide();
-          $("#module_filter_" + type['id']).css('color', '');
+          $(`li.${type['id']}`).hide();
+          $(`#module_filter_${type['id']}`).css('color', '');
         }
       });
     });
