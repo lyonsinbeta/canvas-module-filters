f46ffb9f7b3b3a734a77f165151915d3f773bae6
David Lyons <hello@davidlyons.net>
2020-04-02 16:54:47 -0600

    Quiz filter catches new Quizzes

--- module_filters.js
+++ module_filters.js
@@ -12,20 +12,41 @@ $(document).ready(function() {
                       {id: "context_external_tool", label: "External Tools", icon: "icon-integrations"}];
 
     item_types.forEach(function(type) {
-      var icon = `<i id="module_filter_${type['id']}" class="${type['icon']}" title="${type['label']}" style="color: green;"></i>`;
+      if (type["id"] != "quiz") {
+        var icon = `<i id="module_filter_${type['id']}" class="${type['icon']}" title="${type['label']}" style="color: green;"></i>`;
 
-      $("ul#module_filters").append(`<li style="padding: 0 1em 0 0; display: inline-block;"><input type="checkbox" id="${type['id']}" name="${type['id']}" checked style="display: none;"> <label for="${type['id']}">${icon}</label></li>`);
+        $("ul#module_filters").append(`<li style="padding: 0 1em 0 0; display: inline-block;"><input type="checkbox" id="${type['id']}" name="${type['id']}" checked style="display: none;"> <label for="${type['id']}">${icon}</label></li>`);
 
-      $(`#${type['id']}`).change(function() {
-        if (this.checked == true) {
-          $(`li.${type['id']}`).show();
-          $(`#module_filter_${type['id']}`).css('color', 'green');
-        }
-        else {
-          $(`li.${type['id']}`).hide();
-          $(`#module_filter_${type['id']}`).css('color', '');
-        }
-      });
+        $(`#${type['id']}`).change(function() {
+          if (this.checked == true) {
+            $(`li.${type['id']}`).show();
+            $(`#module_filter_${type['id']}`).css('color', 'green');
+          }
+          else {
+            $(`li.${type['id']}`).hide();
+            $(`#module_filter_${type['id']}`).css('color', '');
+          }
+        });
+      }
+      else {
+        /* Makes the single Quiz filter also catch new Quizzes */
+        var icon = `<i id="module_filter_${type['id']}" class="${type['icon']}" title="${type['label']}" style="color: green;"></i>`;
+
+        $("ul#module_filters").append(`<li style="padding: 0 1em 0 0; display: inline-block;"><input type="checkbox" id="${type['id']}" name="${type['id']}" checked style="display: none;"> <label for="${type['id']}">${icon}</label></li>`);
+
+        $(`#${type['id']}`).change(function() {
+          if (this.checked == true) {
+            $(`li.${type['id']}`).show();
+            $("li.lti-quiz").show();
+            $(`#module_filter_${type['id']}`).css('color', 'green');
+          }
+          else {
+            $(`li.${type['id']}`).hide();
+            $("li.lti-quiz").hide();
+            $(`#module_filter_${type['id']}`).css('color', '');
+          }
+        });
+      }
     });
   }
 });
